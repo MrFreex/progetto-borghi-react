974a0598380e220e737a438b5c7ff5419d5b30cb
MrFreex <filippolissandrin@gmail.com>
2023-01-04 00:51:05 +0100

    Base website

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMountain } from "@fortawesome/free-solid-svg-icons";
+
+import styled from 'styled-components'
+
+import { Container, NavDropdown } from "react-bootstrap";
+
+import { Stack } from "@mui/material";
+
+import NavbarBase from "react-bootstrap/Navbar";
+import NavLink from "react-bootstrap/NavLink";
+import { Link } from "react-router-dom";
+
+const BrandFlex = styled(NavbarBase.Brand)`
+    display: flex;
+    column-gap: 10px;
+
+    & > * {
+        display: block;
+        margin: auto;
+    }
+`
+
+const StyledNavbar = styled(NavbarBase)`
+    background: #ffffff54 !important;
+    -webkit-backdrop-filter: blur(8px);
+    backdrop-filter: blur(8px);
+
+    border-bottom: solid 1px #000000b4;
+`
+
+export const Navbar = () => {
+    return <StyledNavbar fixed="top" bg="light" expand="lg">
+    <Container fluid>
+        <BrandFlex href="#home">
+            <FontAwesomeIcon scale="2x" icon={faMountain} />
+            <span>Comune di Sappada</span>
+        </BrandFlex>
+        <NavbarBase.Toggle aria-controls="basic-navbar-nav" />
+        <NavbarBase.Collapse id="basic-navbar-nav">
+        <Stack direction="row" spacing={2}>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="story">Storia</NavLink>
+          <NavDropdown title="Collegamenti" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Stack>
+      </NavbarBase.Collapse>
+    </Container>
+  </StyledNavbar>
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from 'react-bootstrap';
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+import { Home } from './pages/Home';
+import { Navbar } from './components/Navbar';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<Home />} />
+      {/*
+      <Route path='signup' element={<Signup />} />
+      <Route path='app-info' element={<AppInfo />} />
+      <Route path='verify' element={<Scanned />} />
+      <Route path='panel'>
+        <Route index element={<Panel />} />
+        <Route path='events'>
+          <Route index element={<Events />} />
+          <Route path='edit/:eventId' element={<EditEvent />} />
+        </Route>
+      </Route>
+      */}
+    </Route>
+  )
+);
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    { /* <Container> */ }
+      <RouterProvider router={router} />
+    { /* </Container> */ }
+    { /* Footer */ }
   </React.StrictMode>,
 )
